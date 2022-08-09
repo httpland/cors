f774f2af30c78f0e6ae203bf9f04ab18aa53f278
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2022-08-09 21:38:15 +0900

    chore(_tools): add npm build script

---
+++ _tools/meta.ts
@@ -0,0 +1,39 @@
+import { BuildOptions } from "https://deno.land/x/dnt@0.30.0/mod.ts";
+
+export const makeOptions = (version: string): BuildOptions => ({
+  test: false,
+  shims: {
+    deno: false,
+    undici: true,
+  },
+  typeCheck: true,
+  entryPoints: ["./mod.ts"],
+  outDir: "./npm",
+  package: {
+    name: "@httpland/cors",
+    version,
+    description: "CORS protocol for standard Request and Response",
+    keywords: [
+      "http",
+      "cors",
+      "handler",
+      "request",
+      "response",
+    ],
+    license: "MIT",
+    homepage: "https://github.com/httpland/cors",
+    repository: {
+      type: "git",
+      url: "git+https://github.com/httpland/cors.git",
+    },
+    bugs: {
+      url: "https://github.com/httpland/cors/issues",
+    },
+    sideEffects: false,
+    type: "module",
+    publishConfig: {
+      access: "public",
+    },
+  },
+  packageManager: "pnpm",
+});
